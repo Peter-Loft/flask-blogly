ba0313907ae7f44685f632c3b0c51895c78ad75d
Nate Snyder <n8.snyder@berkeley.edu>
2022-02-02 12:34:28 -0800

    Add routes for homepage and /add_user as well as basic respective templates

--- app.py
+++ app.py
@@ -1,13 +1,22 @@
 """Blogly application."""
 
-from flask import Flask
+from flask import Flask, render_template, request
 from models import db, connect_db, User
 
 app = Flask(__name__)
-app.config['SQLALCHEMY_DATABASE_URI'] = 'postgresql:///blogly'
-app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False
-app.config['SQLALCHEMY_ECHO'] = True
+app.config["SQLALCHEMY_DATABASE_URI"] = "postgresql:///blogly"
+app.config["SQLALCHEMY_TRACK_MODIFICATIONS"] = False
+app.config["SQLALCHEMY_ECHO"] = True
 
 connect_db(app)
 # db.create_all()
 
+
+@app.get("/")
+def users_list():
+    return render_template("users_list.html")
+
+
+@app.get("/add_user")
+def add_user():
+    return render_template("add_user.html")
