560735000e6d589182cfea1890dd5184f35e8686
Peter Loft <peterloft@Peters-MacBook-Pro.local>
2022-02-03 16:10:42 -0600

    created new posts table and ammended the setup_seed

--- models.py
+++ models.py
@@ -1,6 +1,7 @@
 """Models for Blogly."""
 
 from flask_sqlalchemy import SQLAlchemy
+from sqlalchemy.sql import func
 
 db = SQLAlchemy()
 
@@ -11,6 +12,7 @@ def connect_db(app):
     db.app = app
     db.init_app(app)
 
+
 class User(db.Model):
     """User"""
 
@@ -20,6 +22,27 @@ class User(db.Model):
                    primary_key=True,
                    autoincrement=True)
     first_name = db.Column(db.String(50),
-                     nullable=False)
+                           nullable=False)
     last_name = db.Column(db.String(50))
     image_url = db.Column(db.String(100))
+
+
+class Post(db.Model):
+    """Post for Blog"""
+
+    __tablename__ = "posts"
+
+    id = db.Column(db.Integer,
+                   primary_key=True,
+                   autoincrement=True)
+    title = db.Column(db.String(100),
+                      nullable=False)
+    content = db.Column(db.String(2000),
+                        nullable=False)
+    created_at = db.Column(db.DateTime(timezone=True),
+                           server_default=func.now())
+    user_id = db.Column(db.Integer,
+                        db.ForeignKey("users.id"),
+                        nullable=False)
+                        
+    author = db.relationship('User', backref='posts')
--- setup_seed.py
+++ setup_seed.py
@@ -1,6 +1,6 @@
 """Seed file to make sample data for pets db."""
 
-from models import db, User
+from models import db, User, Post
 from app import app
 
 # Create all tables
@@ -8,6 +8,7 @@ db.drop_all()
 db.create_all()
 
 # If table isn't empty, empty it
+Post.query.delete()
 User.query.delete()
 
 # Add users
@@ -23,6 +24,26 @@ spike = User(first_name="Spike", last_name="porcupine")
 db.session.add(whiskey)
 db.session.add(bowser)
 db.session.add(spike)
+
+db.session.commit()
+
+
+# Add posts
+
+whisky_post = Post(
+    title= "Whiskey's Opinion's",
+    content= "I have none.",
+    user_id= whiskey.id
+)
+
+bowser_post = Post(
+    title= "Bowser's Opinion's",
+    content= "I have MANY.",
+    user_id= bowser.id
+)
+
+db.session.add(whisky_post)
+db.session.add(bowser_post)
 
 # Commit--otherwise, this never gets saved!
 db.session.commit()
